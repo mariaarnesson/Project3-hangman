a35337ef0d013cc6ac892869d580ea809c752943
mariaarnesson <maria_arnesson@outlook.com>
2022-12-07 10:37:10 +0000

    Add def menu to run.py file

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def menu():
+    print('WELCOME TO HANGMAN GAME \n')
+
+print('________________________________________________________')
+print('         Hello and welcome to Hangman Game')
+print('_________________________________________________________')
+input('Press Enter to continue') 
+
+menu()   
+
