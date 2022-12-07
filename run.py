5f74dcba1bc7877bb2f29098398519f3e490a07b
mariaarnesson <maria_arnesson@outlook.com>
2022-12-07 10:57:49 +0000

    Add a function hangman

--- run.py
+++ run.py
@@ -13,3 +13,30 @@ input('Press Enter to continue')
 
 menu()   
 
+print(' _______________')
+print('|   Hangman     | ')
+print(' _______________')
+
+def hangman():
+    word_to_guess = ['attraction']
+    trials = 10
+    guessing = ''
+    inscription = set('abcdefghijklmnopqrstuvwxyz')
+
+    while len(word) > 0
+       main_word = ' '
+
+       for letter in word:
+        if letter in guessing:
+            main_word = main_word + letter
+        else:
+            main_word = main_word + '_'
+
+        if main_word == word:
+            print(main_word)
+            print('congratulations! You won!')
+                    
+
+
+
+
