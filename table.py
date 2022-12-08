57a27c81b9356b177d088cd8ba0d31da342fb377
mariaarnesson <maria_arnesson@outlook.com>
2022-12-08 15:10:30 +0000

    Add some changes

--- table.py
+++ table.py
@@ -1,19 +1,69 @@
 import random
-
 def main_menu():
     print("WELCOME TO BATTELSHIP GAME \n")
     
 print("________________________________________________________")
 print("            Hello and Welcome to Battelship game")
 print("________________________________________________________")
+
+print("\n")
+print("             /||                                        ")
+print("            / ||_\                                      ")
+print("           /  ||__\                                     ")
+print("          /   ||___\                                    ")
+print("         /    ||____\                                   ")
+print("        /     ||_____\                                  ")
+print("       /      ||______\                                 ")
+print("      /       ||_______\                                ")
+print("     /        ||        \                               ")
+print("   _/_________||_________\________                      ")
+print("   \_____________________________/                      ")
+print("    \___________________________/                       ")
+print("     \_________________________/                      \n")
+print("                                                      \n")
+print("                                ____||__  _____||__     ")
+print("                       ____||_ )________( )_________(   ")
+print("                      )_______( ____||__  _____||__     ")
+print("                       ____||_ )________( )_________(   ")
+print("                      )_______()________( )_________(   ")
+print("                       ____||_  ____||__  _____||__     ")
+print("                      )_______()________( )_________(   ")
+print("                           ||  )________( )_________(   ")
+print("                         __||______||__________||_____  ")
+print("                         \ // ///  /////   /////  // /  ")
+print("                          \                         /   ")
+print("                           \_______________________/    ")
+print("                        /                                    ")
+print("                       / |                                   ")
+print("                      /  |\                                  ") 
+print("             _______ /  /| \                                 ")
+print("            /______//  / |  \                                 ")
+print("           /______//  /  |   /\\                              ")
+print("                  /  /   | //  \ \                             ")
+print("                 /  /    ///    \  \                           ")
+print("                /  /   ////      \  \                           ")
+print("               /  /  /////        |  \                          ")
+print("              /  / /////|         |\  \                         ")
+print("             /  //////// |       /  \ |                          ")
+print("            /  //_____/  |      /    \                           ")
+print("           /  /  _______ |_____/    /                            ")
+print("       ___/__/___| |__\ |_____/__  /                              ") 
+print("       \_____\____________________/                              ")
+print("        \_____\__________________/                               ")
+print("          \____\_______________/                                  \n")
+print("                                                                  \n")
+
 input("Press Enter to continue")
 main_menu()
+
 
 print("    ____________________________________")
 print("    ____________________________________")      
 print("                   BATTLESHIP           ")
 print("    ____________________________________")
 print("    ____________________________________\n")
+
+
 from itertools import zip_longest
 
 number = ['   ','  1','  2','  3']
@@ -26,226 +76,121 @@ for number,boats, scale,  in zip_longest(number,boats,scale):
     print('______________________________')
     print(number, boats, scale)
     print('______________________________')
+    
+    
+LENGTH_OF_SHIPS = [5,4,2] 
+board = [
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+]
+
+# We want to refer to columns by letter, but Python accesses lists by number. So we define
+# a dictionary to translate letters to the corresponding number. Note that Python lists start in
+# zero, not in one!
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+}
 
 
-LENGTH_OF_SHIPS = [5,4,2]  
-PLAYER_BOARD = [[" "] * 7 for i in range(7)]
-COMPUTER_BOARD = [[" "] * 7 for i in range(7)]
-PLAYER_GUESS_BOARD = [[" "] * 7 for i in range(7)]
-COMPUTER_GUESS_BOARD = [[" "] * 7 for i in range(7)]
-LETTERS_TO_NUMBERS = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6}
+# By writing this as a function, we don't have to repeat it later. It's less code, it makes
+# the rest easier to read, and if we improve this, we have to do it only once!
+def ask_user_for_board_position():
+    column = input("column (A to F):")
+    while column not in "ABCDEF":
+        print("That column is wrong! It should be A, B, C, D, E or F")
+        column = input("column (A to E):")
+
+    row = input("row (1 to 7):")
+    while row not in "1234567":
+        print("That row is wrong! it should be 1, 2, 3, 4, 5, 6, 7")
+        row = input("row (1 to 7):")
+
+    # The code calling this function will receive the values listed in the return statement below
+    # and it can assign it to variables
+    return int(row) - 1, letters_to_numbers[column]
+
 
 def print_board(board):
-    print("__ A_ B_ C_ D_ E_ F_ ")
+    # Show the board, one row at a time
+    print("__ A_ B_ C_ D_ E_ F_")
+    
     row_number = 1
     for row in board:
         print("%d%s" % (row_number, "|_".join(row)))
-        row_number += 1
         
-        
-        
-        
+        row_number = row_number + 1
 
 
-def place_ships(board):
-    
-    for ship_length in LENGTH_OF_SHIPS:
-        
-        while True:
-            if board == COMPUTER_BOARD:
-                orientation, row, column = random.choice(["H", "V"]), random.randint(0,7), random.randint(0,7)
-                if check_ship_fit(ship_length, row, column, orientation):
-                  
-                    if ship_overlaps(board, row, column, orientation, ship_length) == False:
-                       
-                        if orientation == "H":
-                            for i in range(column, column + ship_length):
-                                board[row][i] = "X"
-                        else:
-                            for i in range(row, row + ship_length):
-                                board[i][column] = "X"
-                        break
-            else:
-                place_ship = True
-                print('___________________________________________________')
-                print('Place the ship with a length of ' + str(ship_length))
-                print('___________________________________________________')
-                row, column, orientation = user_input(place_ship)
-                if check_ship_fit(ship_length, row, column, orientation):
-                    
-                        if ship_overlaps(board, row, column, orientation, ship_length) == False:
-                      
-                            if orientation == "H":
-                                for i in range(column, column + ship_length):
-                                    board[row][i] = "X"
-                            else:
-                                for i in range(row, row + ship_length):
-                                    board[i][column] = "X"
-                            print_board(PLAYER_BOARD)
-                            break 
+# We want 5 battleships, so we use a for loop to ask for a ship 5 times!
+for n in range(5):
+    print("Where do you want ship ", n + 1, "?")
+    row_number, column_number = ask_user_for_board_position()
+
+    # Check that there are no repeats
+    if board[row_number][column_number] == 'X':
+        print("That spot already has a battleship in it!")
+
+    board[row_number][column_number] = 'X'
+    print_board(board)
 
 
-def check_ship_fit(SHIP_LENGTH, row, column, orientation):
-    if orientation == "H":
-        if column + SHIP_LENGTH > 7:
-            return False
-        else:
-            return True
+# Now clear the screen, and the other player starts guessing
+print("\n"*50)
+
+guesses_board = [
+   ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+    ['|_','|_','|_','|_','|_','|_'],
+]
+
+
+# Keep playing until we have 5 right guesses
+guesses = 0
+while guesses < 5:
+    print("Guess a battleship location")
+    row_number, column_number = ask_user_for_board_position()
+
+    if guesses_board[row_number][column_number] != ' ':
+        print("You have already guessed that place!")
+        continue
+
+    # Check that there are no repeats
+    if board[row_number][column_number] == 'X':
+        print("HIT!")
+        guesses_board[row_number][column_number] = 'X'
+        guesses = guesses + 1
     else:
-        if row + SHIP_LENGTH > 7:
-            return False
-        else:
-            return True
+        guesses_board[row_number][column_number] = '.'
+        print("MISS!")
 
-
-def ship_overlaps(board, row, column, orientation, ship_length):
-    if orientation == "H":
-        for i in range(column, column + ship_length):
-            if board[row][i] == "X":
-                return True
-    else:
-        for i in range(row, row + ship_length):
-            if board[i][column] == "X":
-                return True
-    return False
-
-
-def user_input(place_ship):
-    if place_ship == True:
-        while True:
-            try: 
-                orientation = input("Enter orientation (H or V): ").upper()
-                if orientation == "H" or orientation == "V":
-                    break
-            except TypeError:
-                print('Enter a valid orientation H or V')
-        while True:
-            try: 
-                row = input("Enter the row 1-7 of the ship: ")
-                if row in '1234567':
-                    row = int(row) - 1
-                    break
-            except ValueError:
-                print('Enter a valid letter between 1-7')
-        while True:
-            try: 
-                column = input("Enter the column of the ship: ").upper()
-                if column in 'ABCDEFG':
-                    column = LETTERS_TO_NUMBERS[column]
-                    break
-            except KeyError:
-                print('Enter a valid letter between A-G')
-        return row, column, orientation 
-    else:
-        while True:
-            try: 
-                row = input("Enter the row 1-7 of the ship: ")
-                if row in '1234567':
-                    row = int(row) - 1
-                    break
-            except ValueError:
-                print('Enter a valid letter between 1-7')
-        while True:
-            try: 
-                column = input("Enter the column of the ship: ").upper()
-                if column in 'ABCDEFG':
-                    column = LETTERS_TO_NUMBERS[column]
-                    break
-            except KeyError:
-                print('Enter a valid letter between A-G')
-        return row, column        
-
-#check if all ships are hit
-def count_hit_ships(board):
-    count = 0
-    for row in board:
-        for column in row:
-            if column == "X":
-                count += 1
-    return count
-
-#user and computer turn
-def turn(board):
-    if board == PLAYER_GUESS_BOARD:
-        row, column = user_input(PLAYER_GUESS_BOARD)
-        if board[row][column] == "-":
-            turn(board)
-        elif board[row][column] == "X":
-            turn(board)
-        elif COMPUTER_BOARD[row][column] == "X":
-            board[row][column] = "X"
-        else:
-            board[row][column] = "-"
-    else:
-        row, column = random.randint(0,7), random.randint(0,7)
-        if board[row][column] == "-":
-            turn(board)
-        elif board[row][column] == "X":
-            turn(board)
-        elif PLAYER_BOARD[row][column] == "X":
-            board[row][column] = "X"
-        else:
-            board[row][column] = "-"
-            
-
-place_ships(COMPUTER_BOARD)
-#print_board(COMPUTER_BOARD)
-print_board(PLAYER_BOARD)
-place_ships(PLAYER_BOARD)
-        
-while True:
-   
-    while True:
-        print(" ___________")
-        print("| Your Turn |")
-        print(" ___________ \n")
-        print(" _______________________")
-        print('|Guess computer location|')
-        print(" _______________________ \n")
-        print_board(PLAYER_GUESS_BOARD)
-        turn(PLAYER_GUESS_BOARD)
-        break
-    if count_hit_ships(PLAYER_GUESS_BOARD) == 10:
-        print("You win!")
-        break   
-   
-    while True:
-        print(" _______________")
-        print("| Computer Turn |")
-        print(" _______________ \n")
-        turn(COMPUTER_GUESS_BOARD)
-        break           
-    print_board(COMPUTER_GUESS_BOARD)   
-    if count_hit_ships(COMPUTER_GUESS_BOARD) == 10:
-        print("Sorry, the computer won.")
-        break
-    
-    
-    
-    
-    def end_screen():
-               print('                      ((  (( ))  ))              ')
-               print('                    (((( ( (    )) ))       ')
-               print('                  ((/   ( (     ))|) ))     ')
-               print('                 (( | _  \/  _  \/| )) ))    ')
-               print('                ))  | 0      0    |  ))((    ')
-               print('                ))) )     _      / (((( ))    ')
-               print('               ((  ( (    __    /((((((((     ')
-               print('                (( ) )  \ ___ / )) ))((\/     ')
-               print('                \/ \/ ( ( |  |  )) \/((       ')
-               print('                       \/ |  |      \/    ') 
-                         
-                                /||\
-'                             /  ||_\
-                             /   ||__\
-                            /    ||___\
-                           /     ||____\
-                          /      ||_____\
-                                 ||______\
-'                                ||_______\
-                                 ||        )
-                      ___________||________)__________
-                      \_____________________________/
-                       \___________________________/
-                        \_________________________/
-    end_sreen()           
+    print_board(guesses_board)
+print("GAME OVER!")
+print('              _______________________ ')
+print('              |      GAME OVER!      |')   
+print('              | Thank you for plaing |')
+print('              _______________________\n  ')
+print('                      ((  (( ))  ))              ')
+print('                    (((( ( (    )) ))       ')
+print('                  ((/   ( (     ))|) ))     ')
+print('                 (( | _  \/  _  \/| )) ))    ')
+print('                ))  | 0      0    |  ))((    ')
+print('                ))) )     _      / (((( ))    ')
+print('               ((  ( (    __    /((((((((     ')
+print('                (( ) )  \ ___ / )) ))((\/     ')
+print('                \/ \/ ( ( |  |  )) \/((       ')
+print('                       \/ |  |      \/    ') 
+               
